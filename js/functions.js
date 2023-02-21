983448da4251c1c67be2f6d5ac273d7448f9c5d3
konstantin arzamastsev <volgavolga1995@yandex.ru>
2023-02-21 23:05:04 +0300

    доделывает 2 дз

--- js/functions.js
+++ js/functions.js
@@ -1,49 +1,52 @@
-function length(str) {
-  if (str >= 18) {
-    return (true);
-  } else {
-    return (false);
+const checkstring = (string, length) => String(string.length <= length);
+
+console.log(checkstring('проверяемая строка', 20));
+console.log(checkstring('проверяемая строка', 18));
+console.log(checkstring('проверяемая строка', 10));
+
+const checkStringIs = (string) => {
+  string = String(string).toLowerCase().replace(/\s/g, '');
+  return string === string.split('').reverse().join('');
+};
+
+console.log(checkStringIs('топот')); // Результат: true - строка является палиндромом
+console.log(checkStringIs('ДовОд')); // Результат: true - несмотря на разный регистр, тоже палиндром
+console.log(checkStringIs('Кекс')); // Результат: false - это не палиндром
+
+const findNumberInStr = (string) => {
+  string = String(string).replace(/[^0-9]/g, '');
+  return parseInt(string, 10);
+}
+
+console.log(findNumberInStr('2023 год'));// Результат: число 2023
+console.log(findNumberInStr('ECMAScript 2022'));// Результат: число 2022
+console.log(findNumberInStr('1 кефир, 0.5 батона')); // Результат: число 105
+console.log(findNumberInStr('а я томат'));// Результат: NaN
+
+
+const createNewString = (string, length, extension) => {
+  if (string.length >= length) {
+    return string;
   }
-}
 
-console.log(length(('проверяемая строка', 20)));
-console.log(length(('проверяемая строка', 18)));
-console.log(length(('проверяемая строка', 10)));
-
-function palindrome(str) {
-  str = str.toLowerCase();
-  return str === str.split('').reverse().join('');
-}
-
-console.log(palindrome('топот')); // Результат: true - строка является палиндромом
-console.log(palindrome('ДовОд')); // Результат: true - несмотря на разный регистр, тоже палиндром
-console.log(palindrome('Кекс')); // Результат: false - это не палиндром
-
-function numFromStr(str) {
-  str = str.replace(/[^0-9]/g, '');
-  if (str.length !== 0) {
-    return str;
+  while (string.length < length) {
+    const extensionLength = length - string.length;
+    string = extension.slice(0, extensionLength) + string;
   }
-  return ('Nan');
+  return string;
 }
 
-console.log(numFromStr('2023 год'));// Результат: число 2023
-console.log(numFromStr('ECMAScript 2022'));// Результат: число 2022
-console.log(numFromStr('1 кефир, 0.5 батона')); // Результат: число 105
-console.log(numFromStr('а я томат'));// Результат: NaN
+// Добавочный символ использован один раз
+console.log(createNewString('1', 2, '0'));// Результат: строка '01'
 
+// Добавочный символ использован три раза
+ console.log(createNewString('1', 4, '0'));// Результат: строка '0001'
 
-// // Добавочный символ использован один раз
-// имя_функции('1', 2, '0');// Результат: строка '01'
+// Добавочные символы обрезаны с конца
+ console.log(createNewString('q', 4, 'werty'));// Результат: строка 'werq'
 
-// // Добавочный символ использован три раза
-// имя_функции('1', 4, '0');// Результат: строка '0001'
+// Добавочные символы использованы полтора раза
+ console.log(createNewString('q', 4, 'we'));// Результат: строка 'wweq'
 
-// // Добавочные символы обрезаны с конца
-// имя_функции('q', 4, 'werty');// Результат: строка 'werq'
-
-// // Добавочные символы использованы полтора раза
-// имя_функции('q', 4, 'we');// Результат: строка 'wweq'
-
-// // Добавочные символы не использованы, исходная строка не изменена
-// имя_функции('qwerty', 4, '0'); // Результат: строка 'qwerty'
+// Добавочные символы не использованы, исходная строка не изменена
+console.log(createNewString('qwerty', 4, '0')); // Результат: строка 'qwerty'
